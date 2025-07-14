22fc171e2e092cfce2554b6a2e836e12c3c625ec
AjeeshaShaji <ajeeshashaji2357@gmail.com>
2025-07-14 10:05:50 +0530

    file

---
+++ d1.js
@@ -0,0 +1,42 @@
+// console.log(a);
+// console.log(b);
+
+// function fn()
+// {
+//     console.log(a);
+//     console.log(b);
+
+//     {
+//         a=10;
+//         b=10;
+//         console.log(a);
+//         console.log(b);
+
+//     }
+//     var a;
+//     let b;
+// }
+
+
+// {
+//     // a=10;
+//     // b=10;
+
+//     // console.log(a);
+//     // // console.log(b);
+
+//     // var a=10;
+//     // let b;
+
+// }
+
+
+{
+    const a=10;
+    console.log(a);
+
+
+    a=20;
+    console.log(a);
+
+}
---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log("hey")
+
+
+var a=30;
+var b=20;
+
+var s=a+b;
+console.log(s,"this is the sum");
+
+d=10;
+console.log(d);
